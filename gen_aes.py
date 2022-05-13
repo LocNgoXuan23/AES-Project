99a1077dd943f0c6a2dead6c471300903ed8219b
LocNgoXuan23 <>
2022-05-13 13:18:21 +0700

    first commit

--- gen_aes.py
+++ gen_aes.py
@@ -4,25 +4,27 @@ from sklearn.utils import shuffle
 import random
 random.seed(111)
 
-def genAES(numOGen, fileName):
-    plaintext = "0123456789abcdeffedcba9876543210"
-    key = "0f1571c947d9e8590cb7add6af7f6798"
+def genOneTime(plaintext, key):
+    cirpher = encryption(plaintext, key)
+    plain = decryption(cirpher, key)
 
-    # cirpher = encryption(plaintext, key)
-    # plain = decryption(cirpher, key)
+    print('plaintext : ', plaintext)
+    print('key : ', key)
 
-    # print('plaintext : ', plaintext)
-    # print('key : ', key)
+    print('cirpher : ', cirpher)
+    print('plain : ', plain)
 
-    # print('cirpher : ', cirpher)
-    # print('plain : ', plain)
-
+def genAES(numOGen, fileName, plaintext, key):
+    # GET ALL PERMUTATIONS OF PLAINTEXT, KEY AND SHUFFLE 
     all_plaintext = allPermutations(plaintext, 4)
-    plaintext_list = concatenatePermutationList(all_plaintext, 8)
-    print(len(plaintext_list))
-
     all_key = allPermutations(key, 4)
+    all_plaintext, all_key = shuffle(all_plaintext, all_key, random_state=10)
+
+    # CONCAT PERMUTATIONS TO GET 128 BIT AND SHUFFLE
+    plaintext_list = concatenatePermutationList(all_plaintext, 8)
     key_list = concatenatePermutationList(all_key, 8)
+
+    print(len(plaintext_list))
     print(len(key_list))
 
     plaintext_list, key_list = shuffle(plaintext_list, key_list, random_state=10)
@@ -43,4 +45,9 @@ def genAES(numOGen, fileName):
             if not conti:
                 break
 
+
+if __name__ == '__main__':
+    plaintext = "0123456789abcdeffedcba9876543210"
+    key = "0f1571c947d9e8590cb7add6af7f6798"
+    genOneTime(plaintext, key)
 
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from gen_aes import genAES
 
 if __name__ == '__main__':
-    genAES(8000, 'my_cipher.txt')
+    plaintext = "0123456789abcdeffedcba9876543210"
+    key = "0f1571c947d9e8590cb7add6af7f6798"
+    print("START !!!")
+    genAES(8000, 'my_cipher_2.txt', plaintext, key)
+    print("DONE !!!")
